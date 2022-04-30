359c13c7e9524cd8f33d148936f6ef63efca15d8
Vegard Veiset <veiset@gmail.com>
2022-05-01 00:30:50 +0200

    Search query tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,222 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Dispatch, SetStateAction} from 'react';
 import './App.css';
+import socketRed from './img/red-socket.png';
+import socketGreen from './img/green-socket.png';
+import socketBlue from './img/blue-socket.png';
+import socketAny from './img/any-socket.png';
+import socketLink from './img/link.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {addExpression, gemStr, generateStr, movementStr, PoeStringSettings, simplify} from "./OutputString";
+
+const maxLenght = 50;
+
+const App = () => {
+
+    const [rrr, setRrr] = React.useState(false);
+    const [ggg, setGgg] = React.useState(false);
+    const [bbb, setBbb] = React.useState(false);
+
+    const [rrA, setRrA] = React.useState(false);
+    const [ggA, setGgA] = React.useState(false);
+    const [bbA, setBbA] = React.useState(false);
+
+    const [rrg, setRrg] = React.useState(false);
+    const [rrb, setRrb] = React.useState(false);
+    const [ggr, setGgr] = React.useState(false);
+    const [ggb, setGgb] = React.useState(false);
+    const [bbr, setBbr] = React.useState(false);
+    const [bbg, setBbg] = React.useState(false);
+
+    const [rgb, setRgb] = React.useState(false);
+
+    const [rr, setRr] = React.useState(false);
+    const [gg, setGg] = React.useState(false);
+    const [bb, setBb] = React.useState(false);
+
+
+    const [anyThreeLink, setAnyThreeLink] = React.useState(false);
+    const [movement10, setMovement10] = React.useState(false);
+    const [movement15, setMovement15] = React.useState(false);
+
+    const [lightning, setLightning] = React.useState(false);
+    const [fire, setFire] = React.useState(false);
+    const [cold, setCold] = React.useState(false);
+    const [phys, setPhys] = React.useState(false);
+    const [chaos, setChaos] = React.useState(false);
+    const [anyGem, setAnyGem] = React.useState(false);
+
+    const [copied, setCopied] = React.useState<string | undefined>(undefined);
+
+
+    let settings: PoeStringSettings = {
+        anyThreeLink,
+        movement: {
+            ten: movement10,
+            fifteen: movement15,
+        },
+        colors: {
+            rrr, ggg, bbb,
+            rrA, ggA, bbA,
+            ggr, ggb, rrg, rrb, bbg, bbr,
+            rgb,
+            rr, gg, bb
+        },
+        plusGems: {
+            lightning,
+            fire,
+            cold,
+            phys,
+            chaos,
+            any: anyGem,
+        }
+    };
+
+    const result = addExpression(addExpression(
+        simplify(generateStr(settings)),
+        movementStr(settings)
+    ), gemStr(settings));
+
+    return (
+        <div className="wrapper">
+            <div className="container">
+                <div className="item-wide info-header">Path of Exile - Vendor search tool</div>
+                <div className="item-wide">
+                    <div className="result-box">
+                        <div className={result.length > maxLenght ? "result" : result === copied ? "result copied-good" : "result"}>
+                            {result}
+                            {result.length > maxLenght && <div className="error">Error: more than 50 characters, cannot be used in the PoE client</div>}
+                            {result.length <= maxLenght && result.length > 0 && <div className="size-info">length: {result.length}</div>}
+                        </div>
+                        <div className="copy">
+                            <button className="copy-button" onClick={() => {
+                                setCopied(result);
+                                navigator.clipboard.writeText(result);
+                            }}>
+                                Copy
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="break"/>
+                <div className="item">
+                    <div className="column-header">Link colors (3L)</div>
+                    <SocketCheckbox label="Any 3 link" value={anyThreeLink} onChange={setAnyThreeLink} link="*-*-*"/>
+                    <SocketCheckbox label="r-r-*" value={rrA} onChange={setRrA}/>
+                    <SocketCheckbox label="g-g-*" value={ggA} onChange={setGgA}/>
+                    <SocketCheckbox label="b-b-*" value={bbA} onChange={setBbA}/>
+
+                    <SocketCheckbox className="small-padding" label="r-r-r" value={rrr} onChange={setRrr}/>
+                    <SocketCheckbox label="r-r-g" value={rrg} onChange={setRrg}/>
+                    <SocketCheckbox label="r-r-b" value={rrb} onChange={setRrb}/>
+
+                    <SocketCheckbox className="small-padding" label="g-g-g" value={ggg} onChange={setGgg}/>
+                    <SocketCheckbox label="g-g-r" value={ggr} onChange={setGgr}/>
+                    <SocketCheckbox label="g-g-b" value={ggb} onChange={setGgb}/>
+
+                    <SocketCheckbox className="small-padding" label="b-b-b" value={bbb} onChange={setBbb}/>
+                    <SocketCheckbox label="b-b-r" value={bbr} onChange={setBbr}/>
+                    <SocketCheckbox label="b-b-g" value={bbg} onChange={setBbg}/>
+
+                    <SocketCheckbox className="small-padding" label="r-g-b" value={rgb} onChange={setRgb}/>
+                </div>
+                <div className="item">
+                    <div className="column-header"> Movement speed </div>
+                    <Checkbox label="Movement speed (10%)" value={movement10} onChange={setMovement10}/>
+                    <Checkbox label="Movement speed (15%)" value={movement15} onChange={setMovement15}/>
+
+                    <div className="column-header small-padding"> Link colors (2L) </div>
+                    <SocketCheckbox label="r-r" value={rr} onChange={setRr}/>
+                    <SocketCheckbox label="g-g" value={gg} onChange={setGg}/>
+                    <SocketCheckbox label="b-b" value={bb} onChange={setBb}/>
+
+                </div>
+                <div className="item">
+                    <div className="column-header">
+                        Misc
+                    </div>
+                    <Checkbox label="+1 wand (any)" value={anyGem} onChange={setAnyGem}/>
+                    <Checkbox label="+1 lightning wand" value={lightning} onChange={setLightning}/>
+                    <Checkbox label="+1 fire wand" value={fire} onChange={setFire}/>
+                    <Checkbox label="+1 cold wand" value={cold} onChange={setCold}/>
+                    <Checkbox label="+1 phys wand" value={phys} onChange={setPhys}/>
+                    <Checkbox label="+1 chaos wand" value={chaos} onChange={setChaos}/>
+                </div>
+
+                <div className="break"/>
+
+                <div className="item-wide">
+                    <div className="calc-info">
+                        size: {result.length} <br/>
+                        <br/>
+                        colors raw: {generateStr(settings)} <br/>
+                        colors min: {simplify(generateStr(settings))} <br/>
+                        raw c size: {generateStr(settings).length} <br/>
+                        min c size: {simplify(generateStr(settings)).length}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+interface CheckboxProps {
+    label: string
+    value: boolean
+    onChange: Dispatch<SetStateAction<boolean>>
+}
+
+interface LinkCheckboxProps {
+    label: string
+    link?: string
+    value: boolean
+    onChange: Dispatch<SetStateAction<boolean>>
+    className?: string
+}
+
+const Checkbox = (props: CheckboxProps) => {
+    return (
+        <div>
+            <label className="checkbox">
+                <input className="checkbox-input" type="checkbox" checked={props.value} onChange={e => props.onChange(e.target.checked)}/>
+                <span>{props.label}</span>
+            </label>
+        </div>
+    );
+}
+
+const SocketCheckbox = (props: LinkCheckboxProps) => {
+    const els = props.link ? props.link.split("") : props.label.split("");
+
+    return (
+        <div className={props.className}>
+            <label className="checkbox">
+                <input className="checkbox-input" type="checkbox" checked={props.value} onChange={e => props.onChange(e.target.checked)}/>
+                {els.map(c =>
+                    <img className="socket-size" src={imgFromChar(c)} alt="red"/>
+                )}
+                <span className="link-text">
+                {props.label.replaceAll("-", "")}
+                </span>
+            </label>
+        </div>
+    );
+}
+
+function imgFromChar(c: string) {
+    switch (c) {
+        case "r":
+            return socketRed;
+        case "g":
+            return socketGreen;
+        case "b":
+            return socketBlue;
+        case "-":
+            return socketLink;
+        default:
+            return socketAny;
+    }
 }
 
 export default App;
---
+++ src/OutputString.ts
@@ -0,0 +1,178 @@
+export interface PoeStringSettings {
+    anyThreeLink: boolean
+    movement: {
+        ten: boolean
+        fifteen: boolean
+    }
+    colors: {
+        rrr: boolean
+        ggg: boolean
+        bbb: boolean
+
+        rrA: boolean
+        ggA: boolean
+        bbA: boolean
+
+        ggr: boolean
+        ggb: boolean
+        rrg: boolean
+        rrb: boolean
+        bbg: boolean
+        bbr: boolean
+
+        rgb: boolean
+
+        rr: boolean
+        gg: boolean
+        bb: boolean
+    }
+    plusGems: {
+        lightning: boolean
+        fire: boolean
+        cold: boolean
+        phys: boolean
+        chaos: boolean
+        any: boolean
+    }
+}
+
+export function generateStr(settings: PoeStringSettings): string {
+    const colors = settings.colors;
+    const {rrr, ggg, bbb, rrg, rrb, ggr, ggb, bbr, bbg, rgb, rrA, ggA, bbA, rr, gg, bb} = colors;
+
+    let result = "";
+    if (settings.anyThreeLink) result = addExpression(result, "-[rgb]-");
+    if (rrr) result = addExpression(result, "r-r-r");
+    if (ggg) result = addExpression(result, "g-g-g");
+    if (bbb) result = addExpression(result, "b-b-b");
+
+    if (rrA) result = addExpression(result, twoAndAny("r"))
+    if (ggA) result = addExpression(result, twoAndAny("g"))
+    if (bbA) result = addExpression(result, twoAndAny("b"))
+
+    if (rrg) result = addExpression(result, twoAndOne("r", "g"));
+    if (rrb) result = addExpression(result, twoAndOne("r", "b"));
+    if (ggb) result = addExpression(result, twoAndOne("g", "b"));
+    if (ggr) result = addExpression(result, twoAndOne("g", "r"));
+    if (bbg) result = addExpression(result, twoAndOne("b", "g"));
+    if (bbr) result = addExpression(result, twoAndOne("b", "r"));
+
+    if (rr) result = addExpression(result, "r-r");
+    if (gg) result = addExpression(result, "g-g");
+    if (bb) result = addExpression(result, "b-b");
+
+    if (rgb) result = addExpression(result, "r-g-b|r-b-g|b-r-g|b-g-r|g-r-b|g-b-r");
+    return result;
+}
+
+function twoAndOne(b: string, s2: string): string {
+    return `${b}-${b}-${s2}|${b}-${s2}-${b}|${s2}-${b}-${b}`
+}
+
+function twoAndAny(b: string) {
+    return `${b}-${b}-|-${b}-${b}|${b}-[rgb]-${b}`
+}
+
+export function simplify(search: string): string {
+    let result = search;
+    if (result.includes("|-[rgb]-|") || result.startsWith("-[rgb]-|")) return "-[rgb]-"
+
+    result = simplifyTwoAndTwo(result, "g", "r");
+    result = simplifyTwoAndTwo(result, "r", "b");
+    result = simplifyTwoAndTwo(result, "b", "g");
+
+    result = removeCCCWhenCCA(result, "r", "g", "b");
+    result = removeCCCWhenCCA(result, "g", "b", "g");
+    result = removeCCCWhenCCA(result, "b", "r", "g");
+
+    result = simplifyThreeAndTwoAndAny(result, "r", "g", "b");
+    result = simplifyThreeAndTwoAndAny(result, "g", "r", "b");
+    result = simplifyThreeAndTwoAndAny(result, "b", "g", "r");
+
+    result = simplifyCCCWhenCCB(result, "g", "r");
+    result = simplifyCCCWhenCCB(result, "g", "b");
+    result = simplifyCCCWhenCCB(result, "b", "r");
+    result = simplifyCCCWhenCCB(result, "b", "g");
+    result = simplifyCCCWhenCCB(result, "r", "g");
+    result = simplifyCCCWhenCCB(result, "r", "b");
+
+
+    let unique = Array.from(new Set(result.split("|")));
+    return unique.join("|");
+}
+
+// g-g-g|g-g-r|g-r-g|r-g-g -> g-g-r|g-[rg]-g|r-g-g
+function simplifyCCCWhenCCB(result: string, c: string, c2: string): string {
+    let r = result;
+    const search1 = `${c}-${c}-${c}`;
+    const search2 = `${c}-${c}-${c2}|${c}-${c2}-${c}|${c2}-${c}-${c}`;
+
+    if (result.includes(search1) && result.includes(search2)) {
+        r = r.split("|").filter(v => !v.match(`${search1}|${search2}`)).join("|")
+        r = addExpression(r, `${c}-${c}-${c2}|${c}-[${c}${c2}]-${c}|${c2}-${c}-${c}`);
+    }
+
+    return r;
+}
+
+// r-r-g|r-g-r|g-r-r|g-g-r|g-r-g|g-g-r -> g-[gr]-r|r-[gr]-g
+function simplifyTwoAndTwo(result: string, c: string, c2: string): string {
+    let r = result;
+    const search1 = `${c}-${c}-${c2}|${c}-${c2}-${c}|${c2}-${c}-${c}`;
+    const search2 = `${c2}-${c2}-${c}|${c2}-${c}-${c2}|${c}-${c2}-${c2}`;
+    if (result.includes(search1) && result.includes(search2)) {
+        console.log("includes ofc")
+        r = r.split("|").filter(v => !v.match(`${search1}|${search2}`)).join("|")
+        r = addExpression(r, `${c}-[${c}${c2}]-${c2}|${c2}-[${c}${c2}]-${c}`);
+    }
+    return r;
+}
+
+// r-r-r|r-r-|-r-r|r-[rgb]-r -> r-r-|-r-r|r-[rgb]-r
+function removeCCCWhenCCA(result: string, c: string, c2: string, c3: string): string {
+    let r = result;
+    if (result.includes(`${c}-${c}-|-${c}-${c}|${c}-[rgb]-${c}`)) {
+        const replaceStr = `${c}-${c}-${c}|${c}-${c}-[${c2}${c3}]|[${c2}${c3}]-${c}-${c}|${c}-[${c2}${c3}]-${c}`;
+        r = r.split("|").filter(v => !v.match(new RegExp(replaceStr))).join("|");
+    }
+    return r;
+}
+
+// r-r-r|r-r-g|r-b-r|g-r-r|r-r-b|r-b-r|r... -> r-r-|-r-r|r-[rgb]-r
+// return `${b}-${b}-|-${b}-${b}|${b}-[rgb]-${b}`
+function simplifyThreeAndTwoAndAny(result: string, c: string, c2: string, c3: string): string {
+    let r = result;
+    if (result.includes(`${c}-${c}-${c}`) && result.includes(`${c}-${c}-${c2}`) && result.includes(`${c}-${c}-${c3}`)) {
+        const replaceStr = `${c}-${c}-${c}|${c}-${c}-${c2}|${c}-${c2}-${c}|${c2}-${c}-${c}|${c}-${c}-${c3}|${c}-${c3}-${c}|${c3}-${c}-${c}|${c}-${c}-|-${c}-${c}|${c}-[rgb]-${c}`;
+        r = r.split("|").filter(v => !v.match(new RegExp(replaceStr))).join("|");
+        r = addExpression(r, twoAndAny(c))
+    }
+    return r;
+}
+
+export function addExpression(str: string, textToAdd: string | undefined): string {
+    if (textToAdd === undefined || textToAdd.length === 0) {
+        return str;
+    }
+    return str?.length === 0 ? textToAdd : `${str}|${textToAdd}`;
+}
+
+export function movementStr(settings: PoeStringSettings): string {
+    const {ten, fifteen} = settings.movement;
+    let result = "";
+    if (ten) result = addExpression(result, "nn");
+    if (fifteen) result = addExpression(result, "rint");
+    return result;
+}
+
+export function gemStr(settings: PoeStringSettings): string  {
+    const {lightning, chaos, cold, fire, phys, any} = settings.plusGems;
+    if (any) return "ell Sk"
+    let result = "";
+    if (fire) result = addExpression(result, "me Sha");
+    if (cold) result = addExpression(result, "st Si");
+    if (lightning) result = addExpression(result, "nder");
+    if (chaos) result = addExpression(result, "Lor");
+    if (phys) result = addExpression(result, "itho");
+    return result;
+}
