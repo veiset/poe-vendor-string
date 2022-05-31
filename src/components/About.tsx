493c1a561ab7ff1ac05c4c1ad2b581cbb01b3c28
Vegard Veiset <veiset@gmail.com>
2022-05-31 15:36:16 +0200

    Add link webpage statistics

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,8 @@ const About = () => {
             <div className="container">
                 <div className="item-wide about-info">
                     Feature requests and bugs can be reported <a href="https://github.com/veiset/poe-vendor-string/issues">here</a><br/>
-                    Source code at <a href="https://github.com/veiset/poe-vendor-string">github.com/veiset/poe-vendor-string</a>
+                    Source code at <a href="https://github.com/veiset/poe-vendor-string">github.com/veiset/poe-vendor-string</a><br/>
+                    Webpage stats at <a href="https://plausible.io/poe.re">plausible.io/poe.re</a>
                 </div>
             </div>
         </div>
