a15455da5d57d74f708fafde9061d3fdd0757dc6
Vegard Veiset <veiset@gmail.com>
2023-10-26 17:16:11 +0200

    Update stats link to new stats page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
             <p>
                 Source code at <a className="source-link" href="https://github.com/veiset/poe-vendor-string">github.com/veiset/poe-vendor-string</a><br/>
                 Feature requests, issues and bugs can be reported <a className="source-link" href="https://github.com/veiset/poe-vendor-string/issues">here</a><br/>
-                Webpage stats at <a className="source-link" href="https://plausible.io/poe.re">plausible.io/poe.re</a><br/>
+                Webpage stats at <a className="source-link" href="https://plausible.vz.is/poe.re">plausible.io/poe.re</a><br/>
             </p>
 
         </div>
