bfbb43e226e4e8e53cd166a99b68df179cf86e98
Vegard Veiset <veiset@gmail.com>
2023-12-22 12:28:47 +0100

    Map names, co-authored by @vinhvizg1040 (#107)
    Co-authored-by:  vinhvizg1040 <vinhvizg1040@gmail.com>

--- src/layout/PageLinks.tsx
+++ src/layout/PageLinks.tsx
@@ -1,6 +1,7 @@
 import './PageLinks.css';
 import vendorIcon from '../img/linkicons/fusing.png';
 import mapIcon from '../img/linkicons/crimson_temple_map.png';
+import mapNameIcon from '../img/linkicons/chateau_map.png';
 import flaskIcon from '../img/linkicons/glassblower.png';
 import heistIcon from '../img/linkicons/blueprint.png';
 import expeditionIcon from '../img/linkicons/expeidition_reroll.png';
@@ -50,13 +51,15 @@ const PageLinks = () => {
     <div className="page-links">
       <PageLink text="Vendor" icon={vendorIcon} route={"/"} currentPage={currentPage}/>
       <PageLink text="Map modifiers" icon={mapIcon} route={"/maps"} currentPage={currentPage}/>
+      <PageLink text="Map names" icon={mapNameIcon} route={"/mapnames"} currentPage={currentPage}/>
       <PageLink text="Expedition" icon={expeditionIcon} route={"/expedition"} currentPage={currentPage}/>
       <PageLink text="Heist" icon={heistIcon} route={"/heist"} currentPage={currentPage}/>
       <PageLink text="Flasks" icon={flaskIcon} route={"/flasks"} currentPage={currentPage}/>
       <PageLink text="Bestiary" icon={beastIcon} route={"/beast"} currentPage={currentPage}/>
       <p className="support-coffee">
         <img src={coffeeIcon} alt="buy me a coffee" className="coffee-icon"/>
-        <a className="source-link" target="_blank" href="https://www.buymeacoffee.com/veiset" rel="noreferrer">Support the webpage</a>
+        <a className="source-link" target="_blank" href="https://www.buymeacoffee.com/veiset" rel="noreferrer">Support
+          the webpage</a>
       </p>
     </div>
   </div>)
