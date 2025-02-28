2fd5c902ea16a731886f636d820b660683e5ae68
Vegard Veiset <veiset@gmail.com>
2025-02-28 09:58:04 +0100

    Change league from settlers to Phrecia

--- src/pages/expedition/Expedition.tsx
+++ src/pages/expedition/Expedition.tsx
@@ -30,7 +30,7 @@ import {ProfileContext} from "../../components/profile/ProfileContext";
 
 dayjs.extend(relativeTime);
 
-export const leagueName = "Settlers";
+export const leagueName = "Phrecia";
 
 const allItemsFromGeneratedItems = (baseTypeRegex: { [key: string]: BaseTypeRegex }): Item[] => {
   const baseTypes: string[] = Array.from(Object.keys(baseTypeRegex));
