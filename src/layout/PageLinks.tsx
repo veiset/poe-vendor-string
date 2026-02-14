22257c6925cb0e3d08931733f0f2fc086d405204
Jaap Brasser <noreply@jaapbrasser.com>
2026-02-14 23:25:24 +0300

    Added support for tattoos

--- src/layout/PageLinks.tsx
+++ src/layout/PageLinks.tsx
@@ -9,10 +9,11 @@ import expeditionIcon from '../img/linkicons/expeidition_reroll.png';
 import jewelIcon from '../img/linkicons/cobalt.png';
 import beastIcon from '../img/BestiaryOrbFull.png';
 import scarabIcon from '../img/scarab.png';
+import tattooIcon from '../img/tattoo.png';
 import coffeeIcon from '../img/bmc-logo-no-background.png';
 import githubIcon from '../img/github-mark-white.png';
 import plausibleIcon from '../img/plausible_logo_sm.png';
-import {Link, useLocation} from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
 export interface PageLinkProps {
@@ -23,12 +24,12 @@ export interface PageLinkProps {
 }
 
 export const PageLink = (props: PageLinkProps) => {
-  const {text, icon, route, currentPage} = props;
+  const { text, icon, route, currentPage } = props;
   const classes = route === currentPage ? "page-link page-link-current" : "page-link";
   return (
     <div className={classes}>
       <Link to={route}>
-        <img alt={text + "-icon"} className="page-link-icon" src={icon}/>
+        <img alt={text + "-icon"} className="page-link-icon" src={icon} />
         {text}
       </Link>
     </div>
@@ -47,31 +48,32 @@ const PageLinks = () => {
           Path of Exile 2 Regex
         </a>
       </p>
-      <PageLink text="Vendor" icon={vendorIcon} route={"/"} currentPage={currentPage}/>
-      <PageLink text="Map mods" icon={mapIcon} route={"/maps"} currentPage={currentPage}/>
-      <PageLink text="Items" icon={alterationIcon} route={"/items"} currentPage={currentPage}/>
-      <PageLink text="Map names" icon={mapNameIcon} route={"/mapnames"} currentPage={currentPage}/>
-      <PageLink text="Expedition" icon={expeditionIcon} route={"/expedition"} currentPage={currentPage}/>
-      <PageLink text="Heist" icon={heistIcon} route={"/heist"} currentPage={currentPage}/>
-      <PageLink text="Flasks" icon={flaskIcon} route={"/flasks"} currentPage={currentPage}/>
-      <PageLink text="Bestiary" icon={beastIcon} route={"/beast"} currentPage={currentPage}/>
-      <PageLink text="Scarab" icon={scarabIcon} route={"/scarab"} currentPage={currentPage}/>
-      <PageLink text="Jewel" icon={jewelIcon} route={"/jewel"} currentPage={currentPage}/>
+      <PageLink text="Vendor" icon={vendorIcon} route={"/"} currentPage={currentPage} />
+      <PageLink text="Map mods" icon={mapIcon} route={"/maps"} currentPage={currentPage} />
+      <PageLink text="Items" icon={alterationIcon} route={"/items"} currentPage={currentPage} />
+      <PageLink text="Map names" icon={mapNameIcon} route={"/mapnames"} currentPage={currentPage} />
+      <PageLink text="Expedition" icon={expeditionIcon} route={"/expedition"} currentPage={currentPage} />
+      <PageLink text="Heist" icon={heistIcon} route={"/heist"} currentPage={currentPage} />
+      <PageLink text="Flasks" icon={flaskIcon} route={"/flasks"} currentPage={currentPage} />
+      <PageLink text="Bestiary" icon={beastIcon} route={"/beast"} currentPage={currentPage} />
+      <PageLink text="Tattoo" icon={tattooIcon} route={"/tattoo"} currentPage={currentPage} />
+      <PageLink text="Scarab" icon={scarabIcon} route={"/scarab"} currentPage={currentPage} />
+      <PageLink text="Jewel" icon={jewelIcon} route={"/jewel"} currentPage={currentPage} />
       <p></p>
       <p className="support-link">
-        <img src={githubIcon} alt="issue tracker" className="support-icon"/>
+        <img src={githubIcon} alt="issue tracker" className="support-icon" />
         <a className="source-link" href="https://github.com/veiset/poe-vendor-string/issues"
-           rel="noreferrer">
+          rel="noreferrer">
           Report issue
         </a>
       </p>
       <p className="support-link">
-        <img src={coffeeIcon} alt="buy me a coffee" className="support-icon"/>
+        <img src={coffeeIcon} alt="buy me a coffee" className="support-icon" />
         <a className="source-link" href="https://www.buymeacoffee.com/veiset" rel="noreferrer">
           Buy me a coffee</a>
       </p>
       <p className="support-link">
-        <img src={plausibleIcon} alt="website stats" className="support-icon"/>
+        <img src={plausibleIcon} alt="website stats" className="support-icon" />
         <a className="source-link" href="https://p.vz.is/poe.re" rel="noreferrer">
           Website stats</a>
       </p>
--- src/layout/Pages.tsx
+++ src/layout/Pages.tsx
@@ -1,18 +1,20 @@
 import Heist from "../pages/heist/Heist";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Vendor from "../pages/vendor/Vendor";
 import Flasks from "../pages/flasks/Flasks";
 import Expedition from "../pages/expedition/Expedition";
 import Beast from "../pages/beast/Beast";
-import React, {useEffect, useState} from "react";
-import {loadSettings, selectedProfile} from "../utils/LocalStorage";
-import {ProfileContext} from "../components/profile/ProfileContext";
+import React, { useEffect, useState } from "react";
+import { loadSettings, selectedProfile } from "../utils/LocalStorage";
+import { ProfileContext } from "../components/profile/ProfileContext";
 import MapNames from "../pages/mapnames/MapNames";
 import Scarabs from "../pages/scarab/Scarabs";
 import Jewel from "../pages/jewel/Jewel";
 import OptimizedMapMods from "../pages/maps/OptimizedMapMods";
 import MagicItem from "../pages/magicitem/MagicItem";
 import Item from "../pages/item/Item";
+import Tattoo from "../pages/tattoo/Tattoo";
+
 
 const Pages = () => {
   const [globalProfile, setGlobalProfile] = useState(selectedProfile());
@@ -25,20 +27,22 @@ const Pages = () => {
   }, [globalProfile]);
 
   return (
-    <ProfileContext.Provider value={{globalProfile, setGlobalProfile}}>
+    <ProfileContext.Provider value={{ globalProfile, setGlobalProfile }}>
       <Routes>
-        <Route path="/" element={<Vendor key={"vendor-" + profile.name}/>}/>
-        <Route path="/vendor" element={<Vendor key={"vendor-" + profile.name}/>}/>
-        <Route path="/maps" element={<OptimizedMapMods key={"map-" + profile.name}/>}/>
-        <Route path="/items-old" element= {<MagicItem key={"items-" + profile.name}/>}/>
-        <Route path="/items" element= {<Item key={"items-" + profile.name}/>}/>
-        <Route path="/mapnames" element={<MapNames key={"mapnames-" + profile.name}/>}/>
-        <Route path="/flasks" element={<Flasks key={"flask-" + profile.name}/>}/>
-        <Route path="/heist" element={<Heist key={"heist-" + profile.name}/>}/>
-        <Route path="/expedition" element={<Expedition key={"expedition-" + profile.name}/>}/>
-        <Route path="/beast" element={<Beast key={"beast-" + profile.name}/>}/>
-        <Route path="/scarab" element={<Scarabs key={"scarab-" + profile.name}/>}/>
-        <Route path="/jewel" element={<Jewel key={"jewel-" + profile.name}/>}/>
+        <Route path="/" element={<Vendor key={"vendor-" + profile.name} />} />
+        <Route path="/vendor" element={<Vendor key={"vendor-" + profile.name} />} />
+        <Route path="/maps" element={<OptimizedMapMods key={"map-" + profile.name} />} />
+        <Route path="/items-old" element={<MagicItem key={"items-" + profile.name} />} />
+        <Route path="/items" element={<Item key={"items-" + profile.name} />} />
+        <Route path="/mapnames" element={<MapNames key={"mapnames-" + profile.name} />} />
+        <Route path="/flasks" element={<Flasks key={"flask-" + profile.name} />} />
+        <Route path="/heist" element={<Heist key={"heist-" + profile.name} />} />
+        <Route path="/expedition" element={<Expedition key={"expedition-" + profile.name} />} />
+        <Route path="/beast" element={<Beast key={"beast-" + profile.name} />} />
+        <Route path="/scarab" element={<Scarabs key={"scarab-" + profile.name} />} />
+        <Route path="/tattoo" element={<Tattoo key={"tattoo-" + profile.name} />} />
+        <Route path="/jewel" element={<Jewel key={"jewel-" + profile.name} />} />
+
       </Routes>
     </ProfileContext.Provider>
   );
