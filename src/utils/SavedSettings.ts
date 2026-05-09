97e81813ba1f90a46a3eec89471e91b2ed725c01
Day <ay.dogan@outlook.fr>
2026-05-09 09:53:33 +0200

    feat: exclude valdo by default

--- src/utils/SavedSettings.ts
+++ src/utils/SavedSettings.ts
@@ -1,28 +1,37 @@
 import { ContractLevel } from "../pages/heist/Heist";
 import { heistContractTypes } from "../generated/GeneratedHeist";
-import { ItemCategory, MagicItemType, SelectedMod } from "../pages/magicitem/MagicItem";
+import {
+  ItemCategory,
+  MagicItemType,
+  SelectedMod,
+} from "../pages/magicitem/MagicItem";
 import { Itembase } from "../pages/item/ItemBaseSelector";
 import { RareModSelection } from "../pages/item/RareItemSelect";
 import { SelectedMagicMod } from "../pages/item/MagicItemSelect";
-import {RepoeLanguage, RepoeLanguageKey} from "./Languages";
+import { RepoeLanguage, RepoeLanguageKey } from "./Languages";
 
 export const leagueName = "Mirage";
 export const categories: ItemCategory[] = [
   {
-    name: "One Handed Weapons", itemType: [
+    name: "One Handed Weapons",
+    itemType: [
       { name: "Claws", modKey: "Claws" },
       { name: "Daggers", modKey: "Daggers" },
       { name: "Wands", modKey: "Wands" },
       { name: "One Hand Swords", modKey: "One Hand Swords" },
-      { name: "Thrusting One Hand Swords", modKey: "Thrusting One Hand Swords" },
+      {
+        name: "Thrusting One Hand Swords",
+        modKey: "Thrusting One Hand Swords",
+      },
       { name: "One Hand Axes", modKey: "One Hand Axes" },
       { name: "One Hand Maces", modKey: "One Hand Maces" },
       { name: "Sceptres", modKey: "Sceptres" },
       { name: "Rune Daggers", modKey: "Rune Daggers" },
-    ]
+    ],
   },
   {
-    name: "Two Handed Weapons", itemType: [
+    name: "Two Handed Weapons",
+    itemType: [
       { name: "Bows", modKey: "Bows" },
       { name: "Staves", modKey: "Staves" },
       { name: "Two Hand Swords", modKey: "Two Hand Swords" },
@@ -30,10 +39,11 @@ export const categories: ItemCategory[] = [
       { name: "Two Hand Maces", modKey: "Two Hand Maces" },
       { name: "Warstaves", modKey: "Warstaves" },
       { name: "Fishing Rods", modKey: "Fishing Rods" },
-    ]
+    ],
   },
   {
-    name: "Offhand", itemType: [
+    name: "Offhand",
+    itemType: [
       { name: "Quivers", modKey: "Quivers" },
       { name: "Shields (str)", modKey: "Shields (str)" },
       { name: "Shields (dex)", modKey: "Shields (dex)" },
@@ -44,297 +54,299 @@ export const categories: ItemCategory[] = [
     ],
   },
   {
-    name: "Jewellery", itemType: [
+    name: "Jewellery",
+    itemType: [
       { name: "Amulets", modKey: "Amulets" },
       { name: "Rings", modKey: "Rings" },
       { name: "Belts", modKey: "Belts" },
-    ]
+    ],
   },
   {
-    name: "Gloves", itemType: [
+    name: "Gloves",
+    itemType: [
       { name: "Gloves (str)", modKey: "Gloves (str)" },
       { name: "Gloves (dex)", modKey: "Gloves (dex)" },
       { name: "Gloves (int)", modKey: "Gloves (int)" },
       { name: "Gloves (str / dex)", modKey: "Gloves (str, dex)" },
       { name: "Gloves (str / int)", modKey: "Gloves (str, int)" },
       { name: "Gloves (dex / int)", modKey: "Gloves (dex, int)" },
-    ]
+    ],
   },
   {
-    name: "Boots", itemType: [
+    name: "Boots",
+    itemType: [
       { name: "Boots (str)", modKey: "Boots (str)" },
       { name: "Boots (dex)", modKey: "Boots (dex)" },
       { name: "Boots (int)", modKey: "Boots (int)" },
       { name: "Boots (str / dex)", modKey: "Boots (str, dex)" },
       { name: "Boots (str / int)", modKey: "Boots (str, int)" },
       { name: "Boots (dex / int)", modKey: "Boots (dex, int)" },
-    ]
+    ],
   },
   {
-    name: "Helmets", itemType: [
+    name: "Helmets",
+    itemType: [
       { name: "Helmets (str)", modKey: "Helmets (str)" },
       { name: "Helmets (dex)", modKey: "Helmets (dex)" },
       { name: "Helmets (int)", modKey: "Helmets (int)" },
       { name: "Helmets (str / dex)", modKey: "Helmets (str, dex)" },
       { name: "Helmets (str / int)", modKey: "Helmets (str, int)" },
       { name: "Helmets (dex / int)", modKey: "Helmets (dex, int)" },
-    ]
+    ],
   },
   {
-    name: "Body Armours", itemType: [
+    name: "Body Armours",
+    itemType: [
       { name: "Body Armours (str)", modKey: "Body Armours (str)" },
       { name: "Body Armours (dex)", modKey: "Body Armours (dex)" },
       { name: "Body Armours (int)", modKey: "Body Armours (int)" },
       { name: "Body Armours (str / dex)", modKey: "Body Armours (str, dex)" },
       { name: "Body Armours (str / int)", modKey: "Body Armours (str, int)" },
       { name: "Body Armours (dex / int)", modKey: "Body Armours (dex, int)" },
-    ]
+    ],
   },
   {
-    name: "Flasks", itemType: [
-      { name: "Tinctures", modKey: "Tinctures" },
-    ]
+    name: "Flasks",
+    itemType: [{ name: "Tinctures", modKey: "Tinctures" }],
   },
-]
+];
 
 export interface SavedSettings {
-  language: RepoeLanguageKey
-  name: string
-  version: number
-  beast: BeastSettings
-  heist: HeistSettings
-  flask: FlaskSettings
-  expedition: ExpeditionSettings
-  map: MapSettings
-  item: ItemSettings
-  vendor: VendorSettings
-  mapNames: MapNameSettings
-  scarab: ScarabSettings
-  jewel: JewelSettings
-  itemCrafting: ItemCraftingSettings
-  tattoo: TattooSettings
-  runegraft: RunegraftSettings
+  language: RepoeLanguageKey;
+  name: string;
+  version: number;
+  beast: BeastSettings;
+  heist: HeistSettings;
+  flask: FlaskSettings;
+  expedition: ExpeditionSettings;
+  map: MapSettings;
+  item: ItemSettings;
+  vendor: VendorSettings;
+  mapNames: MapNameSettings;
+  scarab: ScarabSettings;
+  jewel: JewelSettings;
+  itemCrafting: ItemCraftingSettings;
+  tattoo: TattooSettings;
+  runegraft: RunegraftSettings;
 }
 
-
 export interface BeastSettings {
-  includeHarvest: boolean
-  minChaosValue: string
-  maxChaosValue: string
-  menagerieLimit: boolean
-  redBeastsOnly: boolean
+  includeHarvest: boolean;
+  minChaosValue: string;
+  maxChaosValue: string;
+  menagerieLimit: boolean;
+  redBeastsOnly: boolean;
 }
 
 export interface HeistSettings {
-  targetValue: number
-  requireCoinValue: boolean
-  contractLevels: ContractLevel[],
+  targetValue: number;
+  requireCoinValue: boolean;
+  contractLevels: ContractLevel[];
 }
 
 export interface FlaskSettings {
-  selectedPrefix: string[]
-  selectedSuffix: string[]
-  ilevel: string
-  onlyMaxPrefixTierMod: boolean
-  onlyMaxSuffixTierMod: boolean
-  matchBothPrefixAndSuffix: boolean
-  ignoreEffectTiers: boolean
-  matchOpenPrefixSuffix: boolean
+  selectedPrefix: string[];
+  selectedSuffix: string[];
+  ilevel: string;
+  onlyMaxPrefixTierMod: boolean;
+  onlyMaxSuffixTierMod: boolean;
+  matchBothPrefixAndSuffix: boolean;
+  ignoreEffectTiers: boolean;
+  matchOpenPrefixSuffix: boolean;
 }
 
 export interface ExpeditionSettings {
-  selectedBaseTypes: string[]
-  league: string
-  addFillerItems: boolean,
-  minValueToDisplay: number
-  minAddValue: number
+  selectedBaseTypes: string[];
+  league: string;
+  addFillerItems: boolean;
+  minValueToDisplay: number;
+  minAddValue: number;
 }
 
 export interface ItemSettings {
-  synthItem: boolean
-  matchAnyAffix: boolean
-  matchOpenAffix: boolean
-  category: ItemCategory
-  itemType: MagicItemType
-  selected: SelectedMod[]
+  synthItem: boolean;
+  matchAnyAffix: boolean;
+  matchOpenAffix: boolean;
+  category: ItemCategory;
+  itemType: MagicItemType;
+  selected: SelectedMod[];
   customText: {
-    value: string,
-    enabled: boolean,
-  }
+    value: string;
+    enabled: boolean;
+  };
 }
 
 export interface ItemCraftingSettings {
-  itembase: Itembase | undefined
-  selectedRareMods: { [p: string]: RareModSelection }
-  selectedMagicMods: SelectedMagicMod[]
+  itembase: Itembase | undefined;
+  selectedRareMods: { [p: string]: RareModSelection };
+  selectedMagicMods: SelectedMagicMod[];
   rareSettings: {
-    matchAnyMod: boolean,
-    matchPrefixAndSuffix: boolean,
-  }
+    matchAnyMod: boolean;
+    matchPrefixAndSuffix: boolean;
+  };
   magicSettings: {
-    onlyIfBothPrefixAndSuffix: boolean,
-    matchOpenAffix: boolean,
-  }
+    onlyIfBothPrefixAndSuffix: boolean;
+    matchOpenAffix: boolean;
+  };
   customText: {
-    value: string,
-    enabled: boolean,
-  }
+    value: string;
+    enabled: boolean;
+  };
 }
 
 export interface MapSettings {
-  badIds: number[]
-  goodIds: number[]
-  allGoodMods: boolean
-  quantity: string
-  packsize: string
-  itemRarity: string
-  optimizeQuant: boolean
-  optimizePacksize: boolean
-  optimizeQuality: boolean
-  displayNightmareMods: boolean
-  displayAffixBadges: boolean
-  groupByAffix: boolean
-  tradeEightModOnly: boolean
-  tradeExcludeValdo: boolean
+  badIds: number[];
+  goodIds: number[];
+  allGoodMods: boolean;
+  quantity: string;
+  packsize: string;
+  itemRarity: string;
+  optimizeQuant: boolean;
+  optimizePacksize: boolean;
+  optimizeQuality: boolean;
+  displayNightmareMods: boolean;
+  displayAffixBadges: boolean;
+  groupByAffix: boolean;
+  tradeEightModOnly: boolean;
+  tradeExcludeValdo: boolean;
   rarity: {
-    normal: boolean
-    magic: boolean
-    rare: boolean
-    include: boolean
-  },
+    normal: boolean;
+    magic: boolean;
+    rare: boolean;
+    include: boolean;
+  };
   corrupted: {
-    enabled: boolean,
-    include: boolean,
-  }
+    enabled: boolean;
+    include: boolean;
+  };
   unidentified: {
-    enabled: boolean,
-    include: boolean,
-  }
+    enabled: boolean;
+    include: boolean;
+  };
   quality: {
-    regular: string,
-    currency: string,
-    divination: string,
-    rarity: string,
-    packSize: string,
-    scarab: string,
-  }
-  anyQuality: boolean,
+    regular: string;
+    currency: string;
+    divination: string;
+    rarity: string;
+    packSize: string;
+    scarab: string;
+  };
+  anyQuality: boolean;
   customText: {
-    value: string,
-    enabled: boolean,
-  },
-  mapDropChance: string,
+    value: string;
+    enabled: boolean;
+  };
+  mapDropChance: string;
 }
 
 export interface MapT17Settings {
-  mods: string[]
-  quantity: string
-  packsize: string
-  optimizeQuant: boolean
-  optimizePacksize: boolean
+  mods: string[];
+  quantity: string;
+  packsize: string;
+  optimizeQuant: boolean;
+  optimizePacksize: boolean;
 }
 
 export interface MapNameSettings {
-  selected: string[]
-  mapTabSearch: boolean
+  selected: string[];
+  mapTabSearch: boolean;
 }
 
 export interface ScarabSettings {
-  selected: string[]
-  maxPrice: string
-  minPrice: string
+  selected: string[];
+  maxPrice: string;
+  minPrice: string;
 }
 
-
 export interface TattooSettings {
-  minValue: string
-  maxValue: string
+  minValue: string;
+  maxValue: string;
 }
 
 export interface JewelSettings {
-  allMatch: boolean
-  magicOnly: boolean
-  abyssJewel: boolean
-  selectedRegular: string[]
-  selectedAbyss: string[]
-  matchBothPrefixAndSuffix: boolean
-  matchOpenPrefixSuffix: boolean
+  allMatch: boolean;
+  magicOnly: boolean;
+  abyssJewel: boolean;
+  selectedRegular: string[];
+  selectedAbyss: string[];
+  matchBothPrefixAndSuffix: boolean;
+  matchOpenPrefixSuffix: boolean;
 }
 
 export interface VendorSettings {
-  anyThreeLink: boolean
-  anyFourLink: boolean
-  anyFiveLink: boolean
-  anySixLink: boolean
-  anySixSocket: boolean
+  anyThreeLink: boolean;
+  anyFourLink: boolean;
+  anyFiveLink: boolean;
+  anySixLink: boolean;
+  anySixSocket: boolean;
   movement: {
-    ten: boolean
-    fifteen: boolean
-  }
+    ten: boolean;
+    fifteen: boolean;
+  };
   colors: {
-    rrr: boolean
-    ggg: boolean
-    bbb: boolean
+    rrr: boolean;
+    ggg: boolean;
+    bbb: boolean;
 
-    rrA: boolean
-    ggA: boolean
-    bbA: boolean
+    rrA: boolean;
+    ggA: boolean;
+    bbA: boolean;
 
-    ggr: boolean
-    ggb: boolean
-    rrg: boolean
-    rrb: boolean
-    bbg: boolean
-    bbr: boolean
+    ggr: boolean;
+    ggb: boolean;
+    rrg: boolean;
+    rrb: boolean;
+    bbg: boolean;
+    bbr: boolean;
 
-    rgb: boolean
+    rgb: boolean;
 
-    raa: boolean
-    gaa: boolean
-    baa: boolean
+    raa: boolean;
+    gaa: boolean;
+    baa: boolean;
 
-    rr: boolean
-    gg: boolean
-    bb: boolean
+    rr: boolean;
+    gg: boolean;
+    bb: boolean;
 
-    rb: boolean
-    gr: boolean
-    bg: boolean
+    rb: boolean;
+    gr: boolean;
+    bg: boolean;
 
-    specLink: boolean
+    specLink: boolean;
     specLinkColors: {
-      r: number | undefined
-      g: number | undefined
-      b: number | undefined
-    }
-  }
+      r: number | undefined;
+      g: number | undefined;
+      b: number | undefined;
+    };
+  };
   plusGems: {
-    lightning: boolean
-    fire: boolean
-    cold: boolean
-    phys: boolean
-    chaos: boolean
-    any: boolean
-  }
+    lightning: boolean;
+    fire: boolean;
+    cold: boolean;
+    phys: boolean;
+    chaos: boolean;
+    any: boolean;
+  };
   damage: {
-    phys: boolean
-    firemult: boolean
-    coldmult: boolean
-    chaosmult: boolean
-  }
+    phys: boolean;
+    firemult: boolean;
+    coldmult: boolean;
+    chaosmult: boolean;
+  };
   weapon: {
-    sceptre: boolean
-    mace: boolean
-    axe: boolean
-    sword: boolean
-    bow: boolean
-    claw: boolean
-    dagger: boolean
-    staff: boolean
-    wand: boolean
-    shield: boolean
-  }
-  gems: number[]
+    sceptre: boolean;
+    mace: boolean;
+    axe: boolean;
+    sword: boolean;
+    bow: boolean;
+    claw: boolean;
+    dagger: boolean;
+    staff: boolean;
+    wand: boolean;
+    shield: boolean;
+  };
+  gems: number[];
 }
 
 export const defaultSettings: SavedSettings = {
@@ -343,18 +355,17 @@ export const defaultSettings: SavedSettings = {
   version: 1,
   beast: {
     includeHarvest: true,
-    minChaosValue: '',
-    maxChaosValue: '',
+    minChaosValue: "",
+    maxChaosValue: "",
     menagerieLimit: true,
     redBeastsOnly: true,
   },
   heist: {
     targetValue: 0,
     requireCoinValue: false,
-    contractLevels:
-      Object.keys(heistContractTypes)
-        .map((key) => heistContractTypes[key])
-        .map((type) => ({ start: 0, end: 0, type })),
+    contractLevels: Object.keys(heistContractTypes)
+      .map((key) => heistContractTypes[key])
+      .map((type) => ({ start: 0, end: 0, type })),
   },
   flask: {
     selectedPrefix: [],
@@ -383,7 +394,7 @@ export const defaultSettings: SavedSettings = {
     customText: {
       value: "",
       enabled: true,
-    }
+    },
   },
   map: {
     goodIds: [],
@@ -399,7 +410,7 @@ export const defaultSettings: SavedSettings = {
     displayAffixBadges: false,
     groupByAffix: false,
     tradeEightModOnly: false,
-    tradeExcludeValdo: false,
+    tradeExcludeValdo: true,
     rarity: {
       normal: true,
       magic: true,
@@ -478,7 +489,7 @@ export const defaultSettings: SavedSettings = {
         r: undefined,
         g: undefined,
         b: undefined,
-      }
+      },
     },
     plusGems: {
       lightning: false,
@@ -511,7 +522,7 @@ export const defaultSettings: SavedSettings = {
   scarab: {
     selected: [],
     maxPrice: "0.81",
-    minPrice: "0.00"
+    minPrice: "0.00",
   },
   jewel: {
     allMatch: false,
@@ -547,11 +558,11 @@ export const defaultSettings: SavedSettings = {
     minValue: "",
     maxValue: "",
     includeTattoos: false,
-  }
-}
+  },
+};
 
 export interface RunegraftSettings {
-  minValue: string
-  maxValue: string
-  includeTattoos: boolean
+  minValue: string;
+  maxValue: string;
+  includeTattoos: boolean;
 }
